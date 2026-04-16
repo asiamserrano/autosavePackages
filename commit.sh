667e2817312ebdef03ede50a4b84c1e5b5bd02ba
Asia Serrano <asiamserrano@gmail.com>
2026-04-16 11:23:23 -0400

    updates

---
+++ commit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# add specific non-swift files 
+git add .gitignore
+git add commit.sh
+
+#git add autosaveNashuaPackage/*
+#git add corePackage/*
+#git add autosavePackage/*
+
+
+# commit, push, status
+git commit -m "${1:-updates}"  
+git push
+git status
